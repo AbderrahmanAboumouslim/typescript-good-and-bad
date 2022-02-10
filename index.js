fcd3ff7769e23168bb1918d7ec800b8731aecc95
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-10 13:10:15 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// bad
+var multiA = function (a, b) {
+    a + b;
+};
+// bad
+var multiB = function (a, b) {
+    a + b;
+};
+// bad
+var multiC = function (a, b) {
+    return a + b;
+};
---
+++ index.ts
@@ -0,0 +1,14 @@
+// bad
+const multiA = (a: number, b: number) => {
+  a + b;
+};
+
+// bad
+const multiB = (a: number, b: number): number => {
+  a + b;
+};
+
+// bad
+const multiC = (a: number, b: number): number => {
+  return a + b;
+};
